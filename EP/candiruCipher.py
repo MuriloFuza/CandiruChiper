7118f1f08b09f6db32e30467ea17cb1f21d297b8
MuriloFuza <muriloacademix@gmail.com>
2022-11-21 16:20:48 -0400

    quase finalizando a Candiru cipher

--- EP/candiruCipher.py
+++ EP/candiruCipher.py
@@ -12,14 +12,130 @@ class CandiruCipher:
     self.encryptingMessage = ''
     self.table = []
     self.encryptedMessage = ''
-    self.keyRailFence = [4, 3, 1, 2, 5, 6, 7]
+    self.keyGlobal = [4, 3, 1, 2, 5, 6, 7]
+    self.keyFlow = 217
     self.messageRailFence = []
+    self.messagePolyalphabetic = []
+    self.messageFlow = []
     self.calc = 0
+    self.positionLetter = { 
+                           0: 'a',
+                           1: 'b',
+                           2: 'c',
+                           3: 'd',
+                           4: 'e',
+                           5: 'f',
+                           6: 'g',
+                           7: 'h',
+                           8: 'i',
+                           9: 'j',
+                           10 :'k',
+                           11 :'l',
+                           12 :'m',
+                           13 :'n',
+                           14 :'o',
+                           15 :'p',
+                           16 :'q',
+                           17 :'r',
+                           18 :'s',
+                           19 :'t',
+                           20 :'u',
+                           21 :'v',
+                           22 :'w',
+                           23 :'x',
+                           24 :'y',
+                           25 :'z',
+                           26 :' '
+                           }
+    self.table = [
+      #A
+      ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+       's','t', 'u', 'v','w', 'x', 'y', 'z', ' '],
+      #B
+      [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+       'r', 's','t', 'u', 'v','w', 'x', 'y', 'z'],
+      #C
+      ['z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+       'q', 'r', 's','t', 'u', 'v','w', 'x', 'y'],
+      #D
+      ['y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+       'p', 'q', 'r', 's','t', 'u', 'v','w', 'x' ],
+      #E
+      ['x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+       'o', 'p', 'q', 'r','s','t', 'u', 'v','w' ],
+      #F
+      ['w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+       'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v'],
+      #G
+      ['v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+       'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u'],
+      #H
+      ['u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+       'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t'],
+      #I
+      ['t','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+       'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s'],
+      #J
+      ['s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'],
+      #k
+      ['r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'],
+      #L
+      ['q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm', 'n', 'o', 'p'],
+      #M
+      ['p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm', 'n', 'o' ],
+      #N
+      ['o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm', 'n'],
+      #O
+      ['n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l', 'm' ],
+      #P
+      [ 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k', 'l' ],
+      #Q
+      ['l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j', 'k'],
+      #R
+      ['k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       'j'],
+      #S
+      ['j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+       ],
+      #T
+      ['i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+       ],
+      #U
+      ['h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+       ],
+      #V
+      ['g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e', 'f', 
+       ],
+      #W
+      ['f','g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd', 'e',  
+       ],
+      #X
+      ['e', 'f','g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c', 'd',  
+       ],
+      #Y
+      ['d','e', 'f','g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b', 'c',   
+       ],
+      #Z
+      [ 'c', 'd','e', 'f','g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a', 'b',  
+       ],
+      #' '
+      ['b', 'c', 'd','e', 'f','g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s', 't','u','v', 'w' ,'x' ,'y', 'z' ,' ', 'a',  
+       ],
+      
+    ]
   
   def railFenceCipher(self):
-    self.calc = len(self.encryptingMessage) / 7
+    self.calc = len(self.encryptingMessage) / len(self.keyGlobal)
     
-    if(len(self.encryptingMessage) % 7 > 0):
+    if(len(self.encryptingMessage) % len(self.keyGlobal) > 0):
       self.calc =int(self.calc) + 1
 
     self.calc = int(self.calc)
@@ -30,43 +146,102 @@ class CandiruCipher:
     # quebrar em vetores
     for i in range(self.calc):
       temp = []
-      for k in range(len(self.keyRailFence)):
+      for k in range(len(self.keyGlobal)):
         if(j <= limit):
           temp.append(self.encryptingMessage[j])
           j += 1
       self.messageRailFence.append(temp)
       
+    
     # colocar na ordem encriptada
     guard = 0
     for i in range(len(self.messageRailFence)):
       temp = [0]*len(self.messageRailFence[i])
       for k in range((len(self.messageRailFence[i]))):
         if(guard <= limit):
-          key = self.keyRailFence[k]
-          letter = self.messageRailFence[i][k]          
+          key = self.keyGlobal[k]
+          letter = self.messageRailFence[i][k]     
+          
+          # verificando quantas voltas é necessario
+          if(key > len(temp)):
+            guard = 0
+            while(guard == 0):
+              if(key > len(temp)):
+                key = int(key / len(temp))
+              else:
+                guard = 1
+               
           temp[key - 1] = letter
         guard += 1 
       self.messageRailFence[i] = temp
       
-    # concatenar o resultado final
-    temp = ["*"]*len(self.messageRailFence[i])
-    for i in range(len(self.messageRailFence)):
-      for k in range((len(self.messageRailFence[i]))):
-        temp[k] += self.messageRailFence[i][k]
-    temp = "".join(temp)
-    
-    # removendo os * 
-    b = '*'
-    for i in range(0,len(b)):
-      temp = temp.replace(b[i],"")
+    guard = 0
+    while(guard != -1):      
+      if(len(self.messageRailFence[guard]) < len(self.keyGlobal)):
+        limit = len(self.keyGlobal) - len(self.messageRailFence[guard])
+        for i in range(limit):
+           if(len(self.messageRailFence[guard]) < len(self.keyGlobal)):
+             self.messageRailFence[guard].append("*")
+        guard += 1
+      else:
+        guard += 1
       
-    self.messageRailFence = temp        
-    print(self.messageRailFence)
+      if(guard == len(self.messageRailFence)):
+        guard = -1
+                                   
+    # concatenar o resultado final
+    aux = ''
+    count = len(self.messageRailFence[0])
+    for l in self.messageRailFence[1:]:
+      assert len(l) == count
+    n = len(self.messageRailFence)
+    final_len = n * count
+    aux = [self.messageRailFence[i % n][int(i / n)] for i in range(final_len)]
+    aux = "".join(aux)
+    aux = aux.replace('*',"")
+      
+    self.messageRailFence = aux
+    print("RailFence: ", self.messageRailFence)
+    
+    
+  def polyalphabeticCipher(self):
+  
+    aux = []
+    aux = list(self.messageRailFence)
+    limit = len(aux)
+    guard = 0
+    
+    while(guard < limit):
+      for i in range(len(self.keyGlobal)):
+        if(guard < limit):
+          pos = list(self.positionLetter.keys())[list(self.positionLetter.values()).index(aux[guard])]
+          self.messagePolyalphabetic.append(self.table[pos][self.keyGlobal[i]]) 
+          guard += 1
+  
+    self.messagePolyalphabetic = "".join(self.messagePolyalphabetic)
+    print("Polyalphabetic: ", self.messagePolyalphabetic)
+    
+    
+  def flowCipher(self):
+    final = []
+    
+    for c in self.messagePolyalphabetic:
+      result = ord(c) ^ self.keyFlow
+      print(chr(result))
+      final.append(result)
+    print(final)
+  
 
-  def encrypt(self, message):
+  def encrypt(self, message, key):
+    
+    res = key.split(',')
+    res = [int(numeric_string) for numeric_string in res]
+    self.keyGlobal = res
     self.encryptingMessage = message
+    
     self.railFenceCipher()
-    pass
+    self.polyalphabeticCipher()
+    self.flowCipher()
   
   def decrypt(self, messageEncrypted):
     pass
